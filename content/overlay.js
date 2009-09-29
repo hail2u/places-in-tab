91c7f11a1f624484679dfa3811b21e2c89e61345
Kyo Nagashima <kyo@hail2u.net>
2009-09-29 21:30:38 +0900

    add settings extension.places-in-tab.hideOriginal for hide original menuitems (no UI)

--- content/overlay.js
+++ content/overlay.js
@@ -1,6 +1,13 @@
-Components.utils.import("resource://places-in-tab/vars.js");
-
 var placesInTab = {
+  init: function () {
+    var boolHideOriginal = nsPreferences.getBoolPref("extensions.places-in-tab.hideOriginal", false);
+
+    if (boolHideOriginal) {
+      document.getElementById("menu_showAllHistory").style.display = "none";
+      document.getElementById("bookmarksShowAll").style.display    = "none";
+    }
+  },
+
   showBookmarks: function (event) {
     query.leftPane = "AllBookmarks";
     openUILink("chrome://places-in-tab/content/places.xul", event, false, true);
@@ -11,3 +18,6 @@ var placesInTab = {
     openUILink("chrome://places-in-tab/content/places.xul", event, false, true);
   }
 };
+
+Components.utils.import("resource://places-in-tab/vars.js");
+window.addEventListener("load", placesInTab.init, false);
