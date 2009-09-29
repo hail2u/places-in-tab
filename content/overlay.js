120dc45b07d466c6d76fdaab0e56b98c1e18cebe
Kyo Nagashima <kyo@hail2u.net>
2009-09-29 22:36:48 +0900

    add option dislog

--- content/overlay.js
+++ content/overlay.js
@@ -1,10 +1,7 @@
 var placesInTab = {
   init: function () {
-    var boolHideOriginal = nsPreferences.getBoolPref("extensions.places-in-tab.hideOriginal", false);
-
-    if (boolHideOriginal) {
-      document.getElementById("menu_showAllHistory").style.display = "none";
-      document.getElementById("bookmarksShowAll").style.display    = "none";
+    if (nsPreferences.getBoolPref("extensions.places-in-tab.hideOriginal", false)) {
+      placesInTab.hideOriginal(document);
     }
   },
 
@@ -16,6 +13,31 @@ var placesInTab = {
   showHistory: function (event) {
     query.leftPane = "History";
     openUILink("chrome://places-in-tab/content/places.xul", event, false, true);
+  },
+
+  hideOriginal: function (t) {
+    t.getElementById("menu_showAllHistory").style.display = "none";
+    t.getElementById("bookmarksShowAll").style.display    = "none";
+  },
+
+  showOriginal: function (t) {
+    t.getElementById("menu_showAllHistory").style.display = "-moz-box";
+    t.getElementById("bookmarksShowAll").style.display    = "-moz-box";
+  },
+
+  loadPrefs: function () {
+    document.getElementById("prefHideOriginal").checked = nsPreferences.getBoolPref("extensions.places-in-tab.hideOriginal", false);
+  },
+
+  savePrefs: function () {
+    var boolHideOriginal = document.getElementById("prefHideOriginal").checked;
+    nsPreferences.setBoolPref("extensions.places-in-tab.hideOriginal", boolHideOriginal);
+
+    if (boolHideOriginal) {
+      placesInTab.hideOriginal(window.opener.opener.document);
+    } else {
+      placesInTab.showOriginal(window.opener.opener.document);
+    }
   }
 };
 
